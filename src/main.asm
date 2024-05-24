52887dfb88e42cf2218bc36efe08f4dbab3968fa
suzukiplan <suzukiplan.ys@gmail.com>
2024-05-25 06:58:51 +0900

    refactor code

--- src/main.asm
+++ src/main.asm
@@ -2,30 +2,33 @@
 
 org $0000
 
-.init
-    di
-    ld sp, $0000
-    call vdp_init
+.reset
+    di                              ; 割り込みは一切使わない
+    ld sp, $0000                    ; スタックは 0xFFFF から使う
+    call vdp_init                   ; 映像初期化
 
+main_loop:
+    call vdp_vsync_wait             ; 垂直動機
+    call joypad_update              ; ジョイパッド入力を更新（joypad, joypad_prev）
+    call player_move                ; 自機の移動
+    call player_attack              ; 自機の攻撃（アイテム使用）
+    call print_debug                ; デバッグ情報を表示
+    jr main_loop
+
+.end:
+    halt
+
+.print_debug
+    ; ヘッダテキスト表示
     ld hl, $0202
     ld de, hello
     call vdp_print_fg_with_DEHL
     call player_init
 
+    ; 現在のVXを表示
     ld hl, $0302
     ld de, str_vx
     call vdp_print_fg_with_DEHL
-
-    ld hl, $0402
-    ld de, str_vy
-    call vdp_print_fg_with_DEHL
-
-main_loop:
-    call vdp_vsync_wait
-    call joypad_update
-    call player_move
-    call player_attack
-
     ld de, (player_vx)
     ld hl, $0305
     call vdp_print_s16_with_DEHL
@@ -35,6 +38,10 @@ main_loop:
     inc hl
     ld (hl), a
 
+    ; 現在のVYを表示
+    ld hl, $0402
+    ld de, str_vy
+    call vdp_print_fg_with_DEHL
     ld de, (player_vy)
     ld hl, $0405
     call vdp_print_s16_with_DEHL
@@ -43,13 +50,7 @@ main_loop:
     ld (hl), a
     inc hl
     ld (hl), a
-
-    jr main_loop
-
-.end:
-    di
-    halt
-    jp end
+    ret
 
 hello: db "ROGUE LIKE A.RPG PROTOTYPE", 0
 str_vx: db "VX:",0
@@ -59,4 +60,3 @@ str_vy: db "VY:",0
 #include "vdp.asm"
 #include "joypad.asm"
 #include "player.asm"
-
